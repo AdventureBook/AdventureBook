aadeb32253701339d25f185e5b3b89eddf85a362
Matteo Gassend <mgassend@gmail.com>
2022-09-13 10:45:52 +0200

    add: header

---
+++ components/Header.js
@@ -0,0 +1,55 @@
+import Link from "next/link";
+
+const menuItems = [
+  <li>
+    <Link href="/campaigns">Campaigns</Link>
+  </li>,
+];
+
+export default function Header() {
+  return (
+    <header>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menuItems}
+            </ul>
+          </div>
+          <Link href="/">
+            <span className="btn btn-ghost normal-case text-xl">
+              AdventureBook
+            </span>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+        </div>
+        <div className="navbar-end">
+          <Link href="/login">
+            <button className="btn">Get started</button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
